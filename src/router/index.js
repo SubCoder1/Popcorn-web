adf69590a932344b34e17083579c1d3250766873
SubCoder1 <ghoshsoumakanti@gmail.com>
2023-01-06 20:26:17 +0530

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+// Routes to all of the views in Popcorn are defined here.
+
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
