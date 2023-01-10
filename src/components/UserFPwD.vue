cf3221a1d5658a4cc8887456d5dd37b03a3a163b
SubCoder1 <ghoshsoumakanti@gmail.com>
2023-01-10 14:52:26 +0530

    Added user forgot password component

---
+++ src/components/UserFPwD.vue
@@ -0,0 +1,17 @@
+<!-- User Forgot Password component rendered through AuthView -->
+
+<template>
+  <div class="reg-form">
+    <h2>Forgot Password?</h2>
+    <router-link to="/auth/login">back to login</router-link>
+    <p class="mt-3">
+      Create a new one as I don't have the facilities to fix it for now big man.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UserFPwD",
+};
+</script>
