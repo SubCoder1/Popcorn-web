c7f5514e9bbd7cf2a52acd461708b8e940044ecc
SubCoder1 <ghoshsoumakanti@gmail.com>
2023-01-10 15:02:12 +0530

    Made forgot-password text smaller

--- src/components/UserFPwD.vue
+++ src/components/UserFPwD.vue
@@ -4,7 +4,7 @@
   <div class="reg-form">
     <h2>Forgot Password?</h2>
     <router-link to="/auth/login">back to login</router-link>
-    <p class="mt-3">
+    <p class="mt-3 text-sm">
       Create a new one as I don't have the facilities to fix it for now big man.
     </p>
   </div>
