161e84cb0c5abc1e0df5ebe401a25683b006e2dc
SubCoder1 <ghoshsoumakanti@gmail.com>
2023-01-10 14:52:37 +0530

    Added user home page component

---
+++ src/components/UserHome.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="h-100 container-fluid text-center">
+    <h2>Welcome to Popcorn.</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UserHome",
+};
+</script>
