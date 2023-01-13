3dcdc2040538bac3b5e64648389d66627e39ce4f
SubCoder1 <ghoshsoumakanti@gmail.com>
2023-01-13 12:59:25 +0530

    Added global store to handle auth data from API

---
+++ src/stores/auth.store.js
@@ -0,0 +1,79 @@
+import { defineStore } from "pinia";
+import axios from "axios";
+
+export const useUserStore = defineStore({
+  id: "user",
+  state: () => ({
+    user_auth: false,
+    username: "",
+  }),
+  persist: true, // store in localStorage
+  getters: {
+    getUserAuth: (state) => state.user_auth,
+    getUserName: (state) => state.username,
+  },
+  actions: {
+    async login(username, password) {
+      let res = {};
+      // Login API request to server
+      await axios
+        .post(process.env.VUE_APP_LOGIN_API, {
+          username: username,
+          password: password,
+        })
+        .then(() => {
+          // successfully logged-in
+          this.user_auth = true;
+          this.username = username;
+          res["status"] = 200;
+        })
+        .catch((e) => {
+          this.user_auth = false;
+          this.username = "";
+          // error occured
+          res["status"] = 400;
+          if (e.response) {
+            // Server sent a response
+            if (e.response.data.status != 500) {
+              res["error"] = "Username or Password is incorrect.";
+            } else {
+              res["error"] = "Server error occured.";
+            }
+          } else {
+            // Server unreachable
+            res["error"] = "Server unreachable at the moment.";
+          }
+        });
+      return res;
+    },
+    async register(username, password) {
+      let res = {};
+      await axios
+        .post(process.env.VUE_APP_REGISTER_API, {
+          username: username,
+          password: password,
+        })
+        .then(() => {
+          // successful registration
+          this.user_auth = true;
+          this.username = username;
+          res["status"] = 200;
+        })
+        .catch((e) => {
+          this.user_auth = false;
+          this.username = "";
+          // error occured
+          res["status"] = 400;
+          if (e.response) {
+            // Server sent a response
+            // show the first validation issue received from server
+            res["error"] = e.response.data.details.errors[0].message;
+          } else {
+            // Server unreachable
+            res["error"] = "Server unreachable at the moment.";
+          }
+        });
+      return res;
+    },
+  },
+});
