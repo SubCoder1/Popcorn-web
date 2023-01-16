edc8b6b5fd6ecd46eae4ddb060a17a8aed4b8378
SubCoder1 <ghoshsoumakanti@gmail.com>
2023-01-16 15:39:19 +0530

    Added GangCreate component layout

---
+++ src/components/GangCreate.vue
@@ -0,0 +1,16 @@
+<!-- Create Gang form component rendered through HomeView -->
+
+<template>
+  <form class="gang-create-form">
+    <h3>Create a Gang!</h3>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "GangCreate",
+  methods: {},
+};
+</script>
+
+<style scoped lang="css"></style>
