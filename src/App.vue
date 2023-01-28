2b1471a4e9fcff37f87e8e0ee315bc6bd4a016cc
SubCoder1 <ghoshsoumakanti@gmail.com>
2023-01-28 11:59:51 +0530

    Added bolder font

--- src/App.vue
+++ src/App.vue
@@ -69,7 +69,7 @@ export default {
 </script>
 
 <style lang="css">
-@import url("https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400;500&display=swap");
 
 html,
 body {
