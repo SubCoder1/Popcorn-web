0600f6e76bd5b58d16f352ad6a2ff4137b3924e3
SubCoder1 <ghoshsoumakanti@gmail.com>
2023-01-28 18:10:53 +0530

    Added new btn loader, moved errbox to dashboard

--- src/components/gang/GangCreate.vue
+++ src/components/gang/GangCreate.vue
@@ -1,16 +1,6 @@
 <!-- Create Gang form component rendered through HomeView -->
 
 <template>
-  <div class="d-flex justify-content-between">
-    <div class="gang-create-error mt-1 mb-1">
-      <p
-        class="form-errors rounded-md text-sm"
-        v-bind:class="{ 'show-error': showErr }"
-      >
-        {{ formErr }}
-      </p>
-    </div>
-  </div>
   <form @submit.prevent="createGang(false)" class="gang-create-form">
     <div
       class="d-flex align-items-center justify-content-between flex-wrap mb-3"
@@ -21,8 +11,10 @@
         class="form-control text-sm rounded-md input-md"
         placeholder="Name"
         v-model.trim="gang_name"
+        minlength="5"
+        maxlength="20"
         id="gangName"
-        @click="removeErr"
+        @click="removeErr()"
         autocomplete="off"
         required
       />
@@ -36,8 +28,9 @@
         class="form-control text-sm rounded-md input-md"
         placeholder="Passkey"
         v-model="gang_pass_key"
+        minlength="5"
         id="gangPassKey"
-        @click="removeErr"
+        @click="removeErr()"
         required
       />
     </div>
@@ -52,7 +45,7 @@
           placeholder="Passkey"
           v-model.number="gang_member_limit"
           id="gangLimit"
-          @click="removeErr"
+          @click="removeErr()"
           min="2"
           max="10"
           required
@@ -62,13 +55,11 @@
     </div>
     <button
       type="submit"
-      class="btn d-flex align-items-center justify-content-center position-relative btn-md rounded-md text-sm mt-4"
+      class="btn d-flex align-items-center justify-content-center btn-md rounded-md text-sm mt-4"
       :disabled="form_submitted"
     >
-      <div v-if="form_submitted" class="loader-2 position-absolute">
-        <span />
-      </div>
-      Create
+      <div class="loader" v-if="form_submitted"></div>
+      <span v-if="!form_submitted">Create</span>
     </button>
   </form>
 </template>
@@ -80,9 +71,7 @@ import { useAuthStore } from "@/stores/auth.store";
 export default {
   data() {
     return {
-      showErr: false,
       form_submitted: false,
-      formErr: "",
       gang_name: "",
       gang_pass_key: "",
       gang_member_limit: 2,
@@ -160,42 +149,41 @@ export default {
             this.$parent.$parent.$parent.$parent.srvErrModal();
           } else {
             // Maybe validation
-            this.formErr = response.error;
-            this.showErr = true;
+            this.$parent.$parent.ErrPopUp(response.error);
           }
         }
       }
       this.form_submitted = false;
     },
     removeErr: function () {
-      this.showErr = false;
-      this.formErr = "";
+      this.$parent.$parent.removeErr();
     },
     validateForm: function () {
+      let error = "";
       if (this.gang_name.length < 5 || this.gang_name.length > 20) {
         // gang_name length should be between [5,20]
-        this.formErr = "Gang name should be of 5 - 20 characters.";
-        this.showErr = true;
+        error = "Gang name should be of 5 - 20 characters.";
+        this.$parent.$parent.ErrPopUp(error);
         return false;
       } else if (/[^a-zA-Z0-9_.\s]/g.test(this.gang_name)) {
-        this.formErr =
+        error =
           "Gang name can contain only letters, numbers, underscore, period and whitespaces.";
-        this.showErr = true;
+        this.$parent.$parent.ErrPopUp(error);
         return false;
       } else if (/[^\x20-\x7E]/g.test(this.gang_name)) {
         // gang_name should not contain any unprintable ASCII
-        this.formErr = "Gang name cannot contain any weird characters.";
-        this.showErr = true;
+        error = "Gang name cannot contain any weird characters.";
+        this.$parent.$parent.ErrPopUp(error);
         return false;
       } else if (this.gang_name.trim().length == 0) {
         // gang_name should'nt contain only spaces
-        this.formErr = "Gang name cannot contain only whitespaces";
-        this.showErr = true;
+        error = "Gang name cannot contain only whitespaces";
+        this.$parent.$parent.ErrPopUp(error);
         return false;
       } else if (this.gang_pass_key.length < 5) {
         // gang_passkey length should be >= 5
-        this.formErr = "Gang passkey should be of at least 5 characters.";
-        this.showErr = true;
+        error = "Gang passkey should be of at least 5 characters.";
+        this.$parent.$parent.ErrPopUp(error);
         return false;
       }
       return true;
@@ -203,13 +191,3 @@ export default {
   },
 };
 </script>
-
-<style scoped lang="css">
-.btn-md {
-  width: 203px;
-}
-
-.input-xsm {
-  width: 60px;
-}
-</style>
