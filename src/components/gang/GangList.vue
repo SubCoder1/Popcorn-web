8b51564ae4ac591408b38bfb52cbb80ae7a2e227
SubCoder1 <ghoshsoumakanti@gmail.com>
2023-03-22 18:18:06 +0530

    Integrated changes in gang.store with GangList

--- src/components/gang/GangList.vue
+++ src/components/gang/GangList.vue
@@ -1,44 +1,41 @@
 <template>
-  <div class="d-flex flex-column">
+  <div class="d-flex justify-content-between">
     <div
-      class="d-flex flex-row justify-content-between mb-3"
-      v-for="gang in gangStore.getUserGang"
-      :key="gang.gang_admin"
+      class="d-flex br-left ps-3"
+      v-bind:class="{
+        'br-left': !gangStore.getUserGang.is_admin,
+        'admin-br-left': gangStore.getUserGang.is_admin,
+      }"
     >
-      <div
-        class="d-flex br-left ps-3"
-        v-bind:class="{
-          'br-left': !gang.is_admin,
-          'admin-br-left': gang.is_admin,
-        }"
-      >
-        <div class="d-flex flex-column justify-content-center">
-          <p class="text-sm mb-1">{{ gang.gang_name }}</p>
-          <p class="text-sm mb-1 text-secondary">
-            Admin: {{ gang.gang_admin }}
-          </p>
-          <p class="text-sm mb-1 text-secondary">
-            {{ gang.gang_members_count }} / {{ gang.gang_member_limit }} members
-          </p>
-        </div>
-      </div>
-      <div class="d-flex flex-column">
-        <p class="text-xsm text-secondary text-end">
-          {{ gang.gang_created_timeago }}
+      <div class="d-flex flex-column justify-content-center">
+        <p class="text-sm mb-1">
+          {{ gangStore.getUserGang.gang_name }}
         </p>
-        <button
-          v-if="gang.is_admin"
-          type="button"
-          class="btn btn-sm rounded-md text-sm"
-          v-bind:class="{ 'admin-btn': gang.is_admin }"
-          @click="showCustomizeGangOnly(gang)"
-        >
-          Customize
-        </button>
-        <button v-else type="button" class="btn btn-sm rounded-md text-sm">
-          Join
-        </button>
+        <p class="text-sm mb-1 text-secondary">
+          Admin: {{ gangStore.getUserGang.gang_admin }}
+        </p>
+        <p class="text-sm mb-1 text-secondary">
+          {{ gangStore.getUserGang.gang_members_count }} /
+          {{ gangStore.getUserGang.gang_member_limit }} members
+        </p>
       </div>
+    </div>
+    <div class="d-flex flex-column">
+      <p class="text-xsm text-secondary text-end">
+        {{ gangStore.getUserGang.gang_createdaa }}
+      </p>
+      <button
+        v-if="gangStore.getUserGang.is_admin"
+        type="button"
+        class="btn btn-sm rounded-md text-sm"
+        v-bind:class="{ 'admin-btn': gangStore.getUserGang.is_admin }"
+        @click="showCustomizeGangOnly(gangStore.getUserGang)"
+      >
+        Customize
+      </button>
+      <button v-else type="button" class="btn btn-sm rounded-md text-sm">
+        Join
+      </button>
     </div>
   </div>
   <router-link to="" v-if="gangStore.canJoinGang" @click="showGangList()">
