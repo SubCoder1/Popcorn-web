f74032a2902bde5d511d8176cdc514244b89f475
SubCoder1 <ghoshsoumakanti@gmail.com>
2023-09-28 23:16:35 +0530

    Fixed indentation

--- src/App.vue
+++ src/App.vue
@@ -16,8 +16,8 @@
     <div class="modal-dialog modal-dialog-centered">
       <div class="modal-content border-0">
         <div class="modal-body">
-          Looks like its houseful at the moment, Please refresh or try again after
-          sometime.
+          Looks like its houseful at the moment, Please refresh or try again
+          after sometime.
         </div>
         <div class="modal-footer border-0">
           <button
