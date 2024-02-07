9b615d51c8abc2f4e8d03191e739dac69a0d9452
SubCoder1 <ghoshsoumakanti@gmail.com>
2024-02-07 23:50:49 +0530

    Added Invite-via-URL view

---
+++ src/views/InviteViaUrlView.vue
@@ -0,0 +1,27 @@
+<!-- Invite via URL view of Popcorn which GangJoinViaUrl component -->
+
+<template>
+  <div class="d-flex align-items-center justify-content-center m-auto h-100">
+    <div class="join-via-url-view container-style container-shadow p-3">
+      <GangJoinViaUrl />
+    </div>
+  </div>
+</template>
+
+<script>
+import GangJoinViaUrl from "@/components/gang/GangJoinViaUrl.vue";
+
+export default {
+  name: "InviteViaUrlView",
+  components: {
+    GangJoinViaUrl,
+  },
+};
+</script>
+
+<style scoped lang="css">
+.join-via-url-view {
+  height: auto;
+  width: 500px;
+}
+</style>
