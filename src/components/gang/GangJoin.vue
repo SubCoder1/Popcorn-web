e34e658f8e57a96f9fc8d90f279f4235c833b560
SubCoder1 <ghoshsoumakanti@gmail.com>
2024-03-31 19:44:25 +0530

    Improved gang search page

--- src/components/gang/GangJoin.vue
+++ src/components/gang/GangJoin.vue
@@ -60,46 +60,26 @@
     class="d-flex align-items-center justify-content-between flex-wrap pe-4 ps-4 pb-4"
   >
     <label for="gangName" class="text-sm">Search for a gang:</label>
+    <div class="loader" v-show="gang_search.load_search"></div>
     <input
       type="text"
       class="form-control text-sm rounded-md input-md"
       placeholder="Search by gang name"
       v-model.trim="gang_search.gang_name"
-      @keyup="searchWithDelay(false)"
+      @keyup="searchWithDelay(false, 0)"
       autocomplete="off"
       required
     />
   </div>
   <div
     class="search-result-parent d-flex flex-column p-4"
-    id="gang-search-parent"
+    ref="gang_search_box"
+    v-on:scroll="handleSearchGangOnScroll()"
   >
-    <div v-show="load_search">
-      <div class="h-auto d-flex flex-column justify-content-center">
-        <div class="d-flex flex-row mb-3">
-          <div class="skeleton user-prof-skeleton-md rounded-circle me-3"></div>
-          <div class="d-flex flex-column justify-content-center">
-            <div class="skeleton skeleton-text mb-3"></div>
-            <div class="skeleton skeleton-text"></div>
-          </div>
-        </div>
-        <div class="d-flex flex-row mb-3">
-          <div class="skeleton user-prof-skeleton-md rounded-circle me-3"></div>
-          <div class="d-flex flex-column justify-content-center">
-            <div class="skeleton skeleton-text mb-3"></div>
-            <div class="skeleton skeleton-text"></div>
-          </div>
-        </div>
-        <div class="d-flex flex-row mb-2-5">
-          <div class="skeleton user-prof-skeleton-md rounded-circle me-3"></div>
-          <div class="d-flex flex-column justify-content-center">
-            <div class="skeleton skeleton-text mb-3"></div>
-            <div class="skeleton skeleton-text"></div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div v-show="!load_search && searchResult.length == 0" class="text-center">
+    <div
+      v-show="!gang_search.load_search && gang_search.searchResult.size == 0"
+      class="text-center"
+    >
       <img
         class="empty-search-img m-auto"
         src="@/assets/misc/empty-search.png"
@@ -109,45 +89,49 @@
         Your searches will show up here.
       </p>
     </div>
-    <transition-group v-show="searchResult.length != 0" name="fade" tag="div">
+    <transition-group
+      v-show="gang_search.searchResult.size != 0"
+      name="fade"
+      tag="div"
+    >
       <div
         class="d-flex flex-row justify-content-between mb-3"
-        v-for="gang in searchResult"
-        :key="gang.gang_admin"
+        v-for="(gang, index) in gang_search.searchResult"
+        :key="index"
       >
         <div
           class="d-flex ps-3"
           v-bind:class="{
-            'br-left': !gang.is_admin,
-            'admin-br-left': gang.is_admin,
-            'exp-br-left': gang.is_expired != null && gang.is_expired,
+            'br-left': !gang[1].is_admin,
+            'admin-br-left': gang[1].is_admin,
+            'exp-br-left': gang[1].is_expired != null && gang[1].is_expired,
           }"
         >
           <div class="d-flex flex-column justify-content-center">
-            <p class="text-sm mb-1">{{ gang.gang_name }}</p>
+            <p class="text-sm mb-1">{{ gang[1].gang_name }}</p>
             <p class="text-sm mb-1 text-secondary">
-              Admin: {{ gang.gang_admin }}
+              Admin: {{ gang[1].gang_admin }}
             </p>
             <p class="text-sm mb-1 text-secondary">
-              {{ gang.gang_members_count }}
-              / {{ gang.gang_member_limit }} members
+              {{ gang[1].gang_members_count }}
+              / {{ gang[1].gang_member_limit }} members
             </p>
           </div>
         </div>
         <div class="d-flex flex-column">
           <p class="text-xsm text-secondary text-end">
-            {{ gang.gang_created_timeago }}
+            {{ gang[1].gang_created_timeago }}
           </p>
-          <template v-if="!gang.is_admin">
+          <template v-if="!gang[1].is_admin">
             <button
               v-if="
-                gang.gang_members_count < gang.gang_member_limit &&
-                (gang.is_expired == null || !gang.is_expired)
+                gang[1].gang_members_count < gang[1].gang_member_limit &&
+                (gang[1].is_expired == null || !gang[1].is_expired)
               "
               type="button"
               class="btn d-flex align-items-center justify-content-center position-relative btn-sm rounded-md text-sm join-btn"
               :disabled="load_btn"
-              @click="togglePassKeyModal(gang.gang_name, gang.gang_admin)"
+              @click="togglePassKeyModal(gang[1].gang_name, gang[1].gang_admin)"
             >
               Join
             </button>
@@ -176,8 +160,11 @@ export default {
   data() {
     return {
       gang_search: {
+        load_search: false,
         gang_name: "",
         cursor: 0,
+        searchResult: new Map(),
+        timeout: null,
       },
       gang_join: {
         gang_name: "",
@@ -185,10 +172,7 @@ export default {
         gang_pass_key: "",
         passKeyErr: false,
       },
-      timeout: null,
-      load_search: false,
       load_btn: false,
-      searchResult: [],
       showPassKeyModal: false,
     };
   },
@@ -222,20 +206,25 @@ export default {
 
       return res;
     },
-    searchWithDelay: async function (retry) {
-      this.load_search = true;
-      this.searchResult = [];
-      clearTimeout(this.timeout);
+    searchWithDelay: async function (retry, cur) {
+      this.gang_search.load_search = true;
+      this.gang_search.cursor = cur;
+      if (this.gang_search.cursor == 0) {
+        this.gang_search.searchResult.clear();
+      }
+      clearTimeout(this.gang_search.timeout);
 
-      this.timeout = setTimeout(async () => {
+      this.gang_search.timeout = setTimeout(async () => {
         const response = await this.searchAPI();
         if (response.status == 200) {
-          this.load_search = false;
-          this.searchResult = response.searchResult;
-          this.cursor = response.cursor;
+          this.gang_search.load_search = false;
+          this.gang_search.cursor = response.cursor;
+          response.searchResult.forEach((obj) => {
+            this.gang_search.searchResult.set(obj.gang_admin, obj);
+          });
         } else if (response.status == 400) {
           // Bad request - validation issue or no data found
-          this.load_search = false;
+          this.gang_search.load_search = false;
         } else if (response.status == 401) {
           // Unauthorized
           if (retry == false) {
@@ -244,7 +233,7 @@ export default {
             const authStore = useAuthStore();
             const ref_token_resp = await authStore.refreshToken();
             if (ref_token_resp.status == 200) {
-              await this.searchWithDelay(true);
+              await this.searchWithDelay(true, cur);
             }
           } else {
             // Not able to search gang even after refreshing token
@@ -312,23 +301,20 @@ export default {
       this.showPassKeyModal = !this.showPassKeyModal;
       this.gang_join.gang_admin = gang_join_admin;
       this.gang_join.gang_name = gang_join_name;
-      this.gang_join.gang_passkey = "";
+      this.gang_join.gang_pass_key = "";
       if (!this.showPassKeyModal) {
         this.load_btn = false;
       }
     },
-  },
-  async mounted() {
-    // Detect when scrolled to bottom.
-    const listElm = document.querySelector("#gang-search-parent");
-    listElm.addEventListener("scroll", async () => {
+    handleSearchGangOnScroll: async function () {
+      let elem = this.$refs.gang_search_box;
       if (
-        listElm.scrollTop + listElm.clientHeight >= listElm.scrollHeight &&
+        elem.scrollTop + elem.clientHeight + 5 >= elem.scrollHeight &&
         this.gang_search.cursor != 0
       ) {
-        await this.searchWithDelay(false);
+        await this.searchWithDelay(false, this.gang_search.cursor);
       }
-    });
+    },
   },
 };
 </script>
